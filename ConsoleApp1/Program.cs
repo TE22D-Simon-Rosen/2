c86358c258486337628878d34c9135ff5dd2495b
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2024-11-19 10:26:51 +0100

    fdsfsd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Steg 1
+string[] leksaker = {"nerfpistol", "lego", "träsvärd", "rubiks kub", "docka"};
+
+//Steg 2
+//foreach (string leksak in leksaker){
+//    Console.WriteLine(leksak);
+//}
+
+
+//Steg 3
+string[] klasskamrater = {"Blåa Elias", "Långa Elias", "Adam", "Wiggo", "Rasmus"};
+
+//Steg 4
+int[] siffror = {1, 4, 6, 7, 9};
+
+
+//Steg 5
+for (int i = 0; i < 5; i++){
+    Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {siffror[i]}\n");
+
+}
+
+
+//Steg 6
+List<string> cities = new();
+
+
+while (Console.ReadLine() != "exit"){
+    string input = Console.ReadLine();
+    cities.Add(input);
+}
+
+//Steg 7
+foreach (string h in cities){
+    Console.WriteLine(h);
+}
+
+Console.ReadLine();
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b5057964b0ff3e4555c2f921b9f6ad2170876e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
